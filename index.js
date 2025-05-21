e732154329ffa646c48c801e41a94a9c65ad1080
okwoducynthia <okwoducynthia@gmail.com>
2025-05-21 15:14:49 +0100

    portfolio

---
+++ index.js
@@ -0,0 +1,18 @@
+//FOR CONNECT/LET'S TALK BOTTON
+function connect () {
+  alert("Whatsapp: +234-814-753-5731, Email: okwoducynthia@gmail.com")
+}
+
+//FOR EMAIL BUTTON
+function email () {
+  alert("Email: okwoducynthia@gmail.com")
+}
+
+//FOR NAVBAR ICONS
+function github () {
+  alert("Github account: okwoducynthia")
+}
+
+function whatsapp () {
+  alert("Whatsapp: +234-814-753-5731")
+}
